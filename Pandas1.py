06857ae6ccf8c44d6494d2d57555d6c0a229dcf9
Inf366 <96960255+Inf366@users.noreply.github.com>
2023-02-07 22:17:37 -0300

    Add files via upload

---
+++ Pandas1.py
@@ -0,0 +1,26 @@
+# Autor: GSRS
+
+import pandas as pd
+Combus = pd.read_excel("C:\\Users\\Guilherme Souza\\Desktop\\Pandas_Una\\ca-2021-02.xlsx")
+#Pega n linhas
+#print(Combus.head(5))
+#Faz uma análise
+#print(Combus.describe())
+#Números de linhas e colunas
+#print(Combus.shape)
+#Pega informação das colunas
+#print(Combus.info())
+
+#Pós intervalo
+
+#Linhas específicas
+#print(Combus['Revenda'].head(5))
+Combus2 = Combus[['Revenda','Municipio','Produto','Valor de Venda']]
+#print(Combus2.head(10))
+#print(Combus2.loc[3])
+
+#Maior valor
+Combus3 = Combus2.loc[Combus2['Produto']=='GASOLINA']
+#print(Combus3['Valor de Venda'].max())
+
+print(Combus3[['Revenda','Municipio','Valor de Venda']].max())
